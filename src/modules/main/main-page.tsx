548cd4c0f92cf1471667cee0046eda2f4f4bf1c6
anaya-che <anaya.che22@gmail.com>
2022-02-02 14:24:57 +0300

    fix: link to authorization

--- src/modules/main/main-page.tsx
+++ src/modules/main/main-page.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export function MainPage() {
   return <div>Main Page
     <div><Link to="/">Main</Link></div>
-    <div><Link to="/auth">Authorization</Link></div>
+    <div><Link to="/user">Authorization</Link></div>
     <div><Link to="/games/sprint">Sprint</Link></div>
     <div><Link to="/games/audiocall">Audiocall</Link></div>
     <div><Link to="/textbook">Textbook</Link></div>
