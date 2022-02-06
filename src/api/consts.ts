67662e91803564011ff97a2b524232bf49765c6f
anaya-che <anaya.che22@gmail.com>
2022-02-06 18:28:01 +0300

    feat: implement getting words from api

---
+++ src/api/consts.ts
@@ -0,0 +1,1 @@
+export const baseUrl = `https://rs-lang-bg.herokuapp.com/`; // используйте пока свой url во время разработки
---
+++ src/utils/interfaces.ts
@@ -0,0 +1,22 @@
+export interface IWordData {
+  id: string;
+  group: number;
+  page: number;
+  word: string;
+  image: string;
+  audio: string;
+  audioMeaning: string;
+  audioExample: string;
+  textMeaning: string;
+  textExample: string;
+  transcription: string;
+  wordTranslate: string;
+  textMeaningTranslate: string;
+  textExampleTranslate: string;
+}
+
+export interface IWordStore {
+  wordGroup: number;
+  wordPage: number;
+  wordData: IWordData[];
+}
