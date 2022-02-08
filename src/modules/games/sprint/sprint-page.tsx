6e5f0d80404d51d8c45426f1d23e0d770d810fe7
m.filionchyk <maria.filenchik@yandex.by>
2022-02-08 22:09:45 +0300

    feat: change sprint-page

--- src/modules/games/sprint/sprint-page.tsx
+++ src/modules/games/sprint/sprint-page.tsx
@@ -1,3 +1,24 @@
-export function SprintPage() {
-  return <div>Sprint Page</div>
-}
+import { sprintState } from "../../../store/sprint-state";
+import { observer } from 'mobx-react-lite';
+import { Card } from "./components/card";
+
+export const SprintPage= observer(()=> {
+
+  return (
+    <>
+      <div>Sprint Page</div>
+      <Card />
+      <div>{sprintState.category}</div>
+      <button onClick={() => {
+        sprintState.setCategory(0)
+        sprintState.setStateForRound()
+        }}>1</button>
+      <button onClick={() => sprintState.setCategory(1)}>2</button>
+      <button onClick={() => sprintState.setCategory(2)}>3</button>
+      <button onClick={() => sprintState.setCategory(3)}>4</button>
+      <button onClick={() => sprintState.setCategory(4)}>5</button>
+      <button onClick={() => sprintState.setCategory(5)}>6</button>
+    </>
+  
+  )
+})
