edb7fc2a3b5aee907cfeec3278fde4cf29bfab7e
m.filionchyk <maria.filenchik@yandex.by>
2022-02-10 01:40:22 +0300

    refactor: add selected category handler

--- src/modules/games/sprint/sprint-page.tsx
+++ src/modules/games/sprint/sprint-page.tsx
@@ -4,20 +4,21 @@ import { Card } from "./components/card";
 
 export const SprintPage= observer(()=> {
 
+  const startRound = (category: number) => {
+    sprintState.setCategory(category);
+    sprintState.setStateForRound();
+  }
   return (
     <>
       <div>Sprint Page</div>
       <Card />
       <div>{sprintState.category}</div>
-      <button onClick={() => {
-        sprintState.setCategory(0)
-        sprintState.setStateForRound()
-        }}>1</button>
-      <button onClick={() => sprintState.setCategory(1)}>2</button>
-      <button onClick={() => sprintState.setCategory(2)}>3</button>
-      <button onClick={() => sprintState.setCategory(3)}>4</button>
-      <button onClick={() => sprintState.setCategory(4)}>5</button>
-      <button onClick={() => sprintState.setCategory(5)}>6</button>
+      <button onClick={() => startRound(0)}>1</button>
+      <button onClick={() => startRound(1)}>2</button>
+      <button onClick={() => startRound(2)}>3</button>
+      <button onClick={() => startRound(3)}>4</button>
+      <button onClick={() => startRound(4)}>5</button>
+      <button onClick={() => startRound(5)}>6</button>
     </>
   
   )
