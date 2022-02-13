fdb86030959bb7837ff5cb34fa0942a9842f1157
anaya-che <anaya.che22@gmail.com>
2022-02-13 16:22:34 +0300

    feat: add basic layout for user page

--- src/modules/user/user-page.tsx
+++ src/modules/user/user-page.tsx
@@ -1,3 +1,23 @@
+import { userState } from '../../store/user-state';
+import style from './user.module.scss';
+
 export function UserPage() {
-  return <div>User Page</div>
+  return (
+    <>
+      <div>User Page</div>
+      <div className={style.signInContainer}>
+        <label htmlFor="userName">Имя: </label>
+        <input type="text" name="name" id="userName" />
+        <label htmlFor="userEmail">Почта: </label>
+        <input type="email" name="email" id="userEmail" />
+        <label htmlFor="userPassword">Пароль: </label>
+        <input type="password" name="password" id="userPassword" />
+      </div>
+      <div>
+        <button onClick={() => userState.signIn()}>Войти</button>
+        <button onClick={() => userState.registration()}>Регистрация</button>
+      </div>
+      <div id="message"></div>
+    </>
+  );
 }
