b52bbb458847ffca9a4034f77beeae9f5a44fd3b
anaya-che <anaya.che22@gmail.com>
2022-02-17 00:09:12 +0300

    feat: implement function for statistics api

---
+++ src/api/statistics.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { baseUrl, HttpStatus } from '.';
+import { token } from '../store/user-state';
+import { IStatistic, IStatisticOptional } from '../utils/interfaces';
+
+axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+export const getStatistics = async (userId: string) => {
+  return axios
+    .get(`${baseUrl}users/${userId}/statistics`)
+    .then((res) => console.log(res.data))
+    .catch((error) => {
+      if (error.response.status === HttpStatus.UNAUTHORIZED)
+        console.log('Access token is missing or invalid.');
+      if (error.response.status === HttpStatus.BAD_REQUEST)
+        console.log('Bad request.');
+      else {
+        throw new Error(error);
+      }
+    });
+};
+
+export const updateStatistics = async (
+  userId: string,
+  learned: number,
+  optionalInfo: IStatisticOptional
+): Promise<IStatistic | void> => {
+  return axios
+    .put(`${baseUrl}users/${userId}/statistics`, {
+      learnedWords: learned,
+      optional: optionalInfo,
+    })
+    .then((res): Promise<IStatistic> => res.data)
+    .catch((error) => {
+      if (error.response.status === HttpStatus.UNAUTHORIZED)
+        console.log('Access token is missing or invalid.');
+      if (error.response.status === HttpStatus.BAD_REQUEST)
+        console.log('Bad request.');
+      else {
+        throw new Error(error);
+      }
+    });
+};
