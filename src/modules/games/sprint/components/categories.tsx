f79a0881527382de58463c8cd554108d043a8ab5
m.filionchyk <maria.filenchik@yandex.by>
2022-02-16 15:16:31 +0300

    refactor: startRound function moved to helpers

--- src/modules/games/sprint/components/categories.tsx
+++ src/modules/games/sprint/components/categories.tsx
@@ -3,20 +3,14 @@ import { sprintState } from '../../../../store/sprint-state';
 
 export const Categories: React.FC = observer(()=> {
 
-  const startRound = (category: number): void => {
-    sprintState.setCategory(category);
-    sprintState.setStateForRound();
-    sprintState.startGame(true);
-    sprintState.timerHandler();
-  }
   return (
     <div>
-      <button onClick={(): void => startRound(0)}>1</button>
-      <button onClick={(): void => startRound(1)}>2</button>
-      <button onClick={(): void => startRound(2)}>3</button>
-      <button onClick={(): void => startRound(3)}>4</button>
-      <button onClick={(): void => startRound(4)}>5</button>
-      <button onClick={(): void => startRound(5)}>6</button>
+      <button onClick={(): void => sprintState.startRound(0)}>1</button>
+      <button onClick={(): void => sprintState.startRound(1)}>2</button>
+      <button onClick={(): void => sprintState.startRound(2)}>3</button>
+      <button onClick={(): void => sprintState.startRound(3)}>4</button>
+      <button onClick={(): void => sprintState.startRound(4)}>5</button>
+      <button onClick={(): void => sprintState.startRound(5)}>6</button>
     </div>
   )
 })
