7784f183310c3d9f776e4d69cb055a5b1898fc9b
anaya-che <anaya.che22@gmail.com>
2022-02-19 20:03:55 +0300

    fix: bug with percents of wins in statistics

--- src/modules/statistics/components/words.tsx
+++ src/modules/statistics/components/words.tsx
@@ -7,7 +7,7 @@ export const WordsStat: React.FC<{ data: IDailyWordsStatistic }> = (data) => {
       <h3>Статистика по словам</h3>
       <div>Новых слов: {data.data.newWords}</div>
       <div>Изученных слов: {data.data.learnedWords}</div>
-      <div>Процент правильных ответов: {data.data.percentOfAnswers}</div>
+      <div>Процент правильных ответов: {data.data.percentOfAnswers}%</div>
     </div>
   );
 };
