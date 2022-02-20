ee980dffb27139ffe9414a829a1c251aeb9d7fe8
anaya-che <anaya.che22@gmail.com>
2022-02-20 21:38:59 +0300

    feat: add buttons to cards

--- src/modules/textbook/components/card/index.tsx
+++ src/modules/textbook/components/card/index.tsx
@@ -16,70 +16,79 @@ export const Card: React.FC<{ wordInfo: IWordData; cardStyle: string }> = ({
     wordInfo.userWord?.difficulty === 'easy' ? 'learnedCard' : cardStyle;
 
   return (
-    <div
-      className={style.card + ' ' + style[currentStyle]}
-      style={{ backgroundImage: `url(${urlImage})` }}
-    >
-      {userState.isAuthorized && (
-        <div className={style.wordContainer}>
-          <button
-            onClick={() => {
-              textbookState.changeDifficulty(wordInfo.id, 'difficult');
-            }}
-          >
-            сложные
-          </button>
-          <button
-            onClick={() => {
-              textbookState.changeDifficulty(wordInfo.id, 'easy');
-            }}
-          >
-            выучено
-          </button>
-          {wordInfo.userWord?.difficulty !== 'normal' && (
-            <div>{wordInfo.userWord?.difficulty}</div>
-          )}
-        </div>
-      )}
-      {userState.isAuthorized &&
-        wordInfo.userWord &&
-        (wordInfo.userWord?.optional.mistakes !== 0 ||
-          wordInfo.userWord?.optional.wins !== 0) && (
-          <div className={style.wordContainer}>
-            <div>угадано: {wordInfo.userWord?.optional.wins}</div>
-            <div>не угадано: {wordInfo.userWord?.optional.mistakes}</div>
+    <div className={style.card + ' ' + style[currentStyle]}>
+      <div
+        className={style.imageContainer}
+        style={{ backgroundImage: `url(${urlImage})` }}
+      >
+        <div className={style.cardWrapper}>
+          <div className={style.cardButtons}>
+            {userState.isAuthorized && (
+              <>
+                <div
+                  className={
+                    wordInfo.userWord?.difficulty === 'difficult'
+                      ? style.active + ' ' + style.difficultWord
+                      : style.difficultWord
+                  }
+                  onClick={() => {
+                    textbookState.changeDifficulty(wordInfo.id, 'difficult');
+                  }}
+                ></div>
+
+                <div
+                  className={style.easyWord}
+                  onClick={() => {
+                    textbookState.changeDifficulty(wordInfo.id, 'easy');
+                  }}
+                ></div>
+              </>
+            )}
           </div>
-        )}
-      <div className={style.wordInfo}>
-        <div>{wordInfo.word}</div>
-        <div>{wordInfo.transcription}</div>
-        <div>{wordInfo.wordTranslate}</div>
-        <div
-          className={style.playButton}
-          id="play-word-translation"
-          onClick={() =>
-            playAudio(
-              baseUrl + wordInfo.audio,
-              baseUrl + wordInfo.audioMeaning,
-              baseUrl + wordInfo.audioExample
-            )
-          }
-        ></div>
-      </div>
-      <div className={style.textContainer}>
-        <div className={style.meaningInfo}>
-          <div
-            className="text-meaning"
-            dangerouslySetInnerHTML={textMeaning}
-          ></div>
-          <div>{wordInfo.textMeaningTranslate}</div>
+          <div className={style.wordInfo}>
+            <div>{wordInfo.word}</div>
+            <div>{wordInfo.transcription}</div>
+            <div>{wordInfo.wordTranslate}</div>
+            <div
+              className={style.playButton}
+              id="play-word-translation"
+              onClick={() =>
+                playAudio(
+                  baseUrl + wordInfo.audio,
+                  baseUrl + wordInfo.audioMeaning,
+                  baseUrl + wordInfo.audioExample
+                )
+              }
+            ></div>
+          </div>
         </div>
-        <div className={style.exampleInfo}>
-          <div
-            className="text-example"
-            dangerouslySetInnerHTML={textExample}
-          ></div>
-          <div>{wordInfo.textExampleTranslate}</div>
+      </div>
+
+      <div>
+        <div className={style.textContainer}>
+          <div className={style.meaningInfo}>
+            <div
+              className="text-meaning"
+              dangerouslySetInnerHTML={textMeaning}
+            ></div>
+            <div>{wordInfo.textMeaningTranslate}</div>
+          </div>
+          <div className={style.exampleInfo}>
+            <div
+              className="text-example"
+              dangerouslySetInnerHTML={textExample}
+            ></div>
+            <div>{wordInfo.textExampleTranslate}</div>
+          </div>
+          {userState.isAuthorized &&
+            wordInfo.userWord &&
+            (wordInfo.userWord?.optional.mistakes !== 0 ||
+              wordInfo.userWord?.optional.wins !== 0) && (
+              <div className={style.wordContainer}>
+                <div>угадано: {wordInfo.userWord?.optional.wins}</div>
+                <div>не угадано: {wordInfo.userWord?.optional.mistakes}</div>
+              </div>
+            )}
         </div>
       </div>
     </div>
