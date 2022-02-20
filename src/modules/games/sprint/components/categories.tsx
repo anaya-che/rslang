715d3c5fc76bcdf5ffc0fa67c57a45f8f30dc68f
m.filionchyk <maria.filenchik@yandex.by>
2022-02-20 19:47:21 +0300

    feat: add style for game categories

--- src/modules/games/sprint/components/categories.tsx
+++ src/modules/games/sprint/components/categories.tsx
@@ -1,16 +1,24 @@
 import { observer } from 'mobx-react-lite';
+import { Footer } from '../../../../components/footer/footer';
 import { sprintState } from '../../../../store/sprint-state';
 import style from './sprint.module.scss';
 
 export const Categories: React.FC = observer(()=> {
   return (
-    <div className={style.categoriesBtnGroup}>
-      <button className={style.categoriesBtn} onClick={(): void => sprintState.startRound(0)}>1</button>
-      <button className={style.categoriesBtn} onClick={(): void => sprintState.startRound(1)}>2</button>
-      <button className={style.categoriesBtn} onClick={(): void => sprintState.startRound(2)}>3</button>
-      <button className={style.categoriesBtn} onClick={(): void => sprintState.startRound(3)}>4</button>
-      <button className={style.categoriesBtn} onClick={(): void => sprintState.startRound(4)}>5</button>
-      <button className={style.categoriesBtn} onClick={(): void => sprintState.startRound(5)}>6</button>
+    <>
+    <div className={style.categories}>
+      <h1>Спринт</h1>
+      <div className={style.categoriesBtnGroup}>
+        <button className={style.categoriesBtn} onClick={(): void => sprintState.startRound(0)}>1</button>
+        <button className={style.categoriesBtn} onClick={(): void => sprintState.startRound(1)}>2</button>
+        <button className={style.categoriesBtn} onClick={(): void => sprintState.startRound(2)}>3</button>
+        <button className={style.categoriesBtn} onClick={(): void => sprintState.startRound(3)}>4</button>
+        <button className={style.categoriesBtn} onClick={(): void => sprintState.startRound(4)}>5</button>
+        <button className={style.categoriesBtn} onClick={(): void => sprintState.startRound(5)}>6</button>
+      </div>
+      
     </div>
+    <Footer />
+    </>
   )
 })
--- src/modules/games/sprint/sprint-page.tsx
+++ src/modules/games/sprint/sprint-page.tsx
@@ -2,15 +2,29 @@ import { observer } from 'mobx-react-lite';
 import { Link } from 'react-router-dom';
 import { textbookState } from '../../../store';
 import { sprintState } from '../../../store/sprint-state';
+import { Header } from '../../../components/header/header';
 import { Categories } from './components/categories';
 import { Game } from './components/game';
 import style from './components/sprint.module.scss';
 
 export const SprintPage: React.FC = observer(() => {
   return (
-    <>
-      <div>Sprint Page</div>
-      <header>
+    <div className='wrapper'>
+      
+      <Header />
+      <div className={style.sprintPage}>
+        {sprintState.isGame && sprintState.currentWord !== null ? (
+          <Game />
+        ) : (
+          <Categories />
+        )}
+      </div>
+    </div>
+  );
+});
+
+
+{/* <header>
         <button onClick={sprintState.setDefault}>
           <Link to="/">Main</Link>
         </button>
@@ -21,14 +35,4 @@ export const SprintPage: React.FC = observer(() => {
         >
           <Link to={`/textbook/${textbookState.wordGroup + 1}/${textbookState.wordPage + 1}`}>Textbook</Link>
         </button>
-      </header>
-      <div className={style.sprintPage}>
-        {sprintState.isGame && sprintState.currentWord !== null ? (
-          <Game />
-        ) : (
-          <Categories />
-        )}
-      </div>
-    </>
-  );
-});
+      </header> */}
