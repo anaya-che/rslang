b18f0df55449af35cd0b94131e0a05111f144800
anaya-che <anaya.che22@gmail.com>
2022-02-21 01:08:48 +0300

    feat: change style for main page

--- src/modules/main/components/content.tsx
+++ src/modules/main/components/content.tsx
@@ -5,21 +5,21 @@ export const Content = () => {
   return (
     <div className='content'>
       <div className={style.main}>
-        <section>
+        <section className={style.appInfo}>
           <h1><span>RS</span>Lang</h1>
           <div className={style.description}>
             <div className={style.descriptionText}>
               <p>
               Интерактивное приложение для изучения английского языка, которое поможет в лёгкой игровой форме повысить свой уровень английского. Наши преимущества:
               </p>
-              <p><img src="../../../../icons/bullet.svg" width='20px' height='20px' alt="" /> начать занятия можно с любым уровнем знаний!</p>
-              <p><img src="../../../../icons/bullet.svg" width='20px' height='20px' alt="" /> обучение построено на 6 ступенях сложности, выбери свою!</p>
-              <p><img src="../../../../icons/bullet.svg" width='20px' height='20px' alt="" /> увлекательные тренировки помогут улучшить и закрепить свой словарный запас!</p>
-              <p><img src="../../../../icons/bullet.svg" width='20px' height='20px' alt="" /> повторяй сложные слова для быстрого запоминания!</p>
-              <p><img src="../../../../icons/bullet.svg" width='20px' height='20px' alt="" /> отслеживай свой прогресс ежедневно и ставь новые цели!</p>
+              <p><img src="../../../../icons/bullet.svg" width='15px' height='15px' alt="" /> начать занятия можно с любым уровнем знаний!</p>
+              <p><img src="../../../../icons/bullet.svg" width='15px' height='15px' alt="" /> обучение построено на 6 ступенях сложности, выбери свою!</p>
+              <p><img src="../../../../icons/bullet.svg" width='15px' height='15px' alt="" /> увлекательные тренировки помогут улучшить и закрепить свой словарный запас!</p>
+              <p><img src="../../../../icons/bullet.svg" width='15px' height='15px' alt="" /> повторяй сложные слова для быстрого запоминания!</p>
+              <p><img src="../../../../icons/bullet.svg" width='15px' height='15px' alt="" /> отслеживай свой прогресс ежедневно и ставь новые цели!</p>
             </div>
-            <div>
-              <img src="../../../../img/main-page.jpg" width="100%" alt="team" />
+            <div className={style.mainImageContainer}>
+            <img src="../../../../img/main-page.jpg" width="100%" alt="team" />
             </div>
           </div>
         </section>
